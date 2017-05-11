4d6803f220107346a62115ee5a7b9292fdfbba3d
Jinay Patel <jinay@Jinays-MacBook-Air.local>
2017-05-11 22:27:30 +0530

    added overlay to the floor.

--- HV_plane_detect.py
+++ HV_plane_detect.py
@@ -5,171 +5,117 @@ import argparse
 import random
 import os
 
-display_DEBUG = False
-
-def main(filename):
-    img = cv2.imread(filename)
-    if img is None:
-        print "file does not exist"
-        return
-    
-    clean_img = img.copy()
-    # noise removal
-    blur = cv2.medianBlur(img, 3)
-    blur = cv2.GaussianBlur(blur, (3, 3), 0)
-    blur = cv2.blur(blur, (3, 3))
-
-    hsv_image = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-    H, S, V = hsv_image[:, :, 0], hsv_image[:, :, 1], hsv_image[:, :, 2]
-    # V = 0
-    # hsv_image = cv2.merge([H, S, V])
-    # lower = np.array([40,60,60]) #np arrays for upper and lower thresholds
-    # upper = np.array([100,200,230])
-
-    # imgthreshed = cv2.inRange(hsv_image, lower, upper) #threshold imgHSV
-
-    # plt.imshow(imgthreshed, cmap='gray'), plt.show()
-    # exit(1)
-
-    G = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-    G = cv2.bilateralFilter(G, 9, 30, 30)
 
 
-    # blended = (H + S + V) / 2
-    _, G_thresh = cv2.threshold(G, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+class segmentation():
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    G_close = cv2.dilate(G_thresh, kernel, iterations=1)
+    def __init__(self, filename, isDebugWindowsVisible=False, minContourArea=200):
+        self.isDebugWindowsVisible = isDebugWindowsVisible
+        self.filename = filename
+        self.contours = []
+        self.MIN_CONTOUR_AREA_REQUIRED = minContourArea
 
-    G_edges = cv2.Canny(G_close, 50, 100, apertureSize=3)
+    def preprocess(self, filename):
+        self.img = cv2.imread(filename)
+        if self.img is None:
+            print "file does not exist"
+            return
 
-    # # display HSV Channels
-    if display_DEBUG:
-        plt.subplot(221), plt.imshow(G, cmap='gray'), plt.title("G")
-        plt.subplot(222), plt.imshow(G_thresh, cmap='gray'), plt.title("G_thresh")
-        plt.subplot(223), plt.imshow(G_edges, cmap='gray'), plt.title("G_edges")
-        plt.show()
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
 
-    ret, contours, hierarchy = cv2.findContours(G_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    approx_contours = [cv2.approxPolyDP(c, epsilon=3, closed=True) for c in contours]
-    contours = approx_contours
-    filteredContours = []
-    cnt_img = np.zeros(img.shape[:2], dtype=np.uint8)
-    for c in contours:
-        if cv2.contourArea(c) < 100:
-            continue
-        filteredContours.append(c)
+        # noise removal
+        blur = cv2.medianBlur(self.img, 3)
+        blur = cv2.GaussianBlur(blur, (3, 3), 0)
+        blur = cv2.blur(blur, (3, 3))
 
-    cv2.drawContours(cnt_img, filteredContours, -1, 255, thickness=-1)
-    plt.imshow(cnt_img, cmap='gray'), plt.show()
+        # convert to GRAYSCALE
+        self.G = cv2.cvtColor(blur, cv2.COLOR_RGB2GRAY)
 
-    G_close = cv2.dilate(cnt_img, kernel, iterations=3)
-    G_edges = cv2.Canny(G_close, 20, 40, apertureSize=3)
 
-    plt.imshow(G_edges, cmap='gray'), plt.show()
-    width = G_edges.shape[1] - 1
-    height = G_edges.shape[0] - 1
-    edgePoints = []
-    for j in range(0, width, 8):
-        for i in range(height - 20, 0, -1):
-            if G_edges.item(i,j) == 255:
-                edgePoints.append((j,i))
-                break
-        else:
-            edgePoints.append((j,0))
+        # Noise removal
+        G_filtered = cv2.bilateralFilter(self.G, 9, 30, 30)
 
-    for x in range(len(edgePoints)-1):
-        cv2.line(img, edgePoints[x], edgePoints[x+1], (180,255,0), 3)
-    for x in range(len(edgePoints)):
-        cv2.line(img, (x*8, height), edgePoints[x], (170,180,133),1)
+        # threshold
+        _, G_thresh = cv2.threshold(G_filtered, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    plt.subplot(121), plt.imshow(img)
-    plt.show()
+        return G_thresh
 
-    dumpFileName = str(os.path.splitext(os.path.basename(filename))[0]) + "_OUT.jpg"
-    cv2.imwrite(dumpFileName, img)
-    exit(0);
-    # lines = cv2.HoughLines(G_edges,1,np.pi/180,int(img.shape[1] * 0.20))
-    # draw_H = np.zeros(img.shape,dtype='uint8')
-    # draw_V = np.zeros(img.shape,dtype='uint8')
-    # if lines is not None:
-    #     for line in lines[:10]:
-    #         i = 0
-    #         for rho,theta in line:
-    #             theta_in_degree = theta * 180/np.pi
-    #             print "theta for line[", i, "] is", theta_in_degree
-    #             i += 1
-    #             if theta_in_degree > 180 - 65 or theta_in_degree < 65:
-    #                 line_color = (0, 255, 0)
-    #                 draw_on = draw_V
-    #             elif theta_in_degree > 90 - 10 and theta_in_degree < 90 + 10:
-    #                 line_color = (0, 0, 255)
-    #                 draw_on = draw_H
-    #             else:
-    #                 print "continue"
-    #                 continue
-    #             print "drawing line on"
-    #             a = np.cos(theta)
-    #             b = np.sin(theta)
-    #             x0 = a*rho
-    #             y0 = b*rho
-    #             x1 = int(x0 + 1000*(-b))
-    #             y1 = int(y0 + 1000*(a))
-    #             x2 = int(x0 - 1000*(-b))
-    #             y2 = int(y0 - 1000*(a))
-    #             cv2.line(draw_on,(x1,y1),(x2,y2),line_color,2)
-    #             cv2.line(img,(x1,y1),(x2,y2),line_color,2)
-    # else:
-    #     print "no lines found"
-    # if display_DEBUG:
-    #     plt.subplot(221), plt.imshow(img), plt.title("img")
-    #     plt.subplot(222), plt.imshow(draw_H), plt.title("line_H")
-    #     plt.subplot(223), plt.imshow(draw_V), plt.title("line_V")
-    #     plt.subplot(224), plt.imshow( draw_H + draw_V), plt.title("line_H + line_V")
-    #     plt.show()
+    def get_contours(self, image):
+        # Find contours for the Morphed Image
+        ret, t_contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        t_contours = [cv2.approxPolyDP(c, epsilon=2.5, closed=True) for c in t_contours]
+        contour_plot = np.zeros(image.shape[:2], dtype=np.uint8)
+        for c in t_contours:
+            if cv2.contourArea(c) < self.MIN_CONTOUR_AREA_REQUIRED:
+                continue
+            self.contours.append(c)
 
-    lines_V = []
-    lines_H = []
-    lines = cv2.HoughLinesP(G_close,1, np.pi/2, 2, None, minLineLength=10, maxLineGap=2)
-    if lines is not None:
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                angle = round(np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi, 2)
-                # print "angle:", angle
-                if angle < 45 and angle > -45: # Horizontal Lines
-                    lines_H.append(((x1,y1),(x2,y2)))
-                elif (angle > 85 and angle < 95) or (angle > -95 and angle < -85) : # Vertical Lines
-                    lines_V.append(((x1,y1),(x2,y2)))
+        return self.contours
+
+    def floor_segmentation(self):
+
+        # pre-process the input
+        G_thresh = self.preprocess(self.filename)
+
+        filteredContours = self.get_contours(G_thresh)
+
+        contour_plot = np.zeros(G_thresh.shape[:2], dtype=np.uint8)
+        cv2.drawContours(contour_plot, filteredContours, -1, 255, thickness=-1)
+
+        # dilate and edge detection on the detected contour plot in order to fill the gaps
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        G_morph = cv2.dilate(contour_plot, kernel, iterations=1)
+        G_edges = cv2.Canny(G_morph, 40, 100, apertureSize=3)
+
+        if self.isDebugWindowsVisible:
+            plt.subplot(221), plt.title("G"), plt.imshow(self.G, cmap='gray')
+            plt.subplot(222), plt.title("G_thresh"), plt.imshow(G_thresh, cmap='gray')
+            plt.subplot(223), plt.title("contour_plot"), plt.imshow(contour_plot, cmap='gray')
+            plt.subplot(224), plt.title("G_edges (After Contours)"), plt.imshow(G_edges, cmap='gray')
+            plt.show()
+
+        self.floorEdgePoints = []
+        height = G_edges.shape[0] - 1
+        width = G_edges.shape[1] - 1
+        for j in range(0, width, 8):
+            for i in range(height - 20, 0, -1):
+                if G_edges.item(i,j) == 255:
+                    self.floorEdgePoints.append((j,i))
+                    break
+            else:
+                self.floorEdgePoints.append((j,0))
+
+        self.dump_output(self.floorEdgePoints, width, height)
+
+        return self.floorEdgePoints
+
+
+    def dump_output(self, pts, width, height):
+
+        clean_img = self.img.copy()
+        mask = np.zeros(self.img.shape[:2], dtype=np.uint8)
+        for x in range(len(pts)-1):
+            cv2.line(clean_img, pts[x], pts[x+1], (180,255,0), 2)
+            cv2.line(mask, pts[x], pts[x+1], (255, 0, 0), 2)
+
+        mask = cv2.bitwise_not(mask)
+
+        for i in range(0,width):
+            for j in range(0,height):
+                if mask[j,i] != 255:
+                    break
                 else:
-                    continue
-    else:
-        print "no lines found with HoughLineP"
+                    mask[j,i] = 0
+        B = clean_img[:,:,2]
+        B[mask > 0] = 255
 
-    HoughLines_img = img.copy()
-    img_lineV = np.zeros(img.shape, dtype=np.uint8)
-    for ((x1,y1),(x2,y2)) in lines_V:
-        cv2.line(img_lineV,(x1,y1),(x2,y2),(0, 255, 0),2)
-        cv2.line(HoughLines_img,(x1,y1),(x2,y2),(0,255,0),2)
+        dumpFileName = str(os.path.splitext(os.path.basename(self.filename))[0]) + "_OUT.jpg"
+        if self.isDebugWindowsVisible:
+            plt.imshow(clean_img), plt.title(dumpFileName), plt.show()
 
-    img_lineH = np.zeros(img.shape, dtype=np.uint8)
-    for ((x1,y1),(x2,y2)) in lines_H:
-        cv2.line(img_lineH,(x1,y1),(x2,y2),(0,0,255),2)
-        cv2.line(HoughLines_img,(x1,y1),(x2,y2),(0,0,255),2)
-
-    if display_DEBUG:
-        plt.subplot(221), plt.imshow(clean_img), plt.title("clean_img")
-        plt.subplot(222), plt.imshow(HoughLines_img), plt.title("HoughLineP")
-        plt.subplot(223), plt.imshow(img_lineH), plt.title("img_lineH")
-        plt.subplot(224), plt.imshow(img_lineV), plt.title("img_lineV")
-        plt.show()
-
-    output = G.copy()
-    maskH = img_lineH[:,:,2]
-    maskV = img_lineV[:,:,1]
-    output[maskH != 0] = 0
-    output[maskV != 0] = 0
-    plt.imshow(output, cmap='gray'), plt.show()
-    cv2.imwrite("HoughLineP_OUT.jpg", output)
+        clean_img = cv2.cvtColor(clean_img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(dumpFileName, clean_img)
+        del clean_img
 
 
 if __name__ == "__main__":
@@ -178,6 +124,6 @@ if __name__ == "__main__":
     parser.add_argument("--input", help="input file path", default="data/IMG_6857.JPG")
     parser.add_argument("--debug", help="display intermediate outputs", default=1)
     args = parser.parse_args()
-    if args.debug != 0:
-        display_DEBUG = True
-    main(args.input)
+
+    segment = segmentation(filename=args.input, isDebugWindowsVisible=args.debug)
+    floor_boundary_pts = segment.floor_segmentation()
